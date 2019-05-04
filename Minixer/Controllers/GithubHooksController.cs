6a51e35797676b40dfc0137232f73e3ab9be2dd5
Joseph Portaro <jsportaro@outlook.com>
2019-05-04 10:38:42 -0400

    Initial

---
+++ Minixer/Controllers/GithubHooksController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Minixer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubHooksController : ControllerBase
+    {
+        // GET: api/GithubHooks
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GithubHooks/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GithubHooks
+        [HttpPost]
+        public void Post([FromBody] JToken body)
+        {
+            switch (Request.Headers["X-GitHub-Event"])
+            {
+                case "ping":
+
+                    break;
+                case "push":
+
+                    break;
+            }
+        }
+
+        // PUT: api/GithubHooks/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Minixer/Infrastructure/Messaging/Events/PingEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minixer.Infrastructure.Messaging.Events
+{
+    internal class PingEvent
+    {
+        public string Zen { get; set; }
+    }
+}
